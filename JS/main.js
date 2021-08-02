cc16e2a9b87b238c6182f075ffcfa57d8df71576
unknown <cavalcante.nad@gmail.com>
2021-08-02 14:26:02 -0400

    Alterações

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+var nome = "Nádia Cavalcante "
+var idade = 22
+var idade2 = 10
+var frase = "Vou conhecer a Tailândia :D"
+// alert("Bem vinda, " + nome + ". Você tem " + idade + " anos")
+// alert(idade + idade2)
+// console.log(nome)
+// console.log(idade)
+// console.log(idade + idade2)
+// console.log(frase.replace("Tailândia", "Coréia do Sul"))
+
+console.log(frase.toUpperCase())
+
